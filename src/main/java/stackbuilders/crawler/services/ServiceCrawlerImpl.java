e4968c2a04ce4c11ac2af8230317d0131efe699c
german.simba <german.simba@formos.com>
2020-02-29 14:55:35 -0500

    making method

--- src/main/java/stackbuilders/crawler/services/ServiceCrawlerImpl.java
+++ src/main/java/stackbuilders/crawler/services/ServiceCrawlerImpl.java
@@ -1,10 +1,16 @@
 package stackbuilders.crawler.services;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import stackbuilders.crawler.entities.News;
 
 public class ServiceCrawlerImpl {
 	
@@ -20,7 +26,7 @@ public class ServiceCrawlerImpl {
 	    return response.statusCode();
 	}
 	
-	public static Document getHtmlDocument(String url) {
+	public Document getHtmlDocument(String url) {
 
 	    Document doc = null;
 		try {
@@ -31,4 +37,58 @@ public class ServiceCrawlerImpl {
 	    return doc;
 	} 
 
+	public List<News> loadNews(String url){
+		Document document = getHtmlDocument(url);
+		Elements entradas = document.select(".itemlist");
+		System.out.println("Número de entradas en la página inicial de Jarroba: " + entradas.size() + "\n");
+		List<News> enties = new ArrayList<>();
+		boolean agregar;
+		News newTemp = new News();
+		for (Element table : entradas) {
+
+			for (Element row : table.select("tr")) {
+				Elements tds = row.select("td");
+
+				agregar = false;
+				for (Element td : tds) {
+					if (td.getElementsByClass("rank").size() > 0) {
+						newTemp = new News();
+						String numberOrder = td.getElementsByClass("rank").text();
+						newTemp.setNumberOrder(Integer.parseInt(numberOrder.replace(".", "").trim()));
+
+					}
+					if (td.getElementsByClass("storylink").size() > 0) {
+
+						String titulo = td.getElementsByClass("storylink").text();
+						newTemp.setTitle(titulo);
+					}
+					if (td.getElementsByClass("score").size() > 0) {
+
+						String score = td.getElementsByClass("score").text();
+						newTemp.setPoints(Integer.parseInt(score.replace("points", "").trim()));
+
+						if (td.select("a[href]").size() > 0) {
+							Element refs = td.select("a[href]").get(3);
+//							System.out.println(refs.text());
+							String coment= refs.text().replace("comments", "").replace("comment", "").trim();
+							try {
+							    newTemp.setAmountComments(Integer.parseInt(coment));
+							} catch (NumberFormatException e) {
+								newTemp.setAmountComments(new Integer(0));
+							}
+						}
+						agregar = true;
+					}
+
+				}
+				if (agregar) {
+					enties.add(newTemp);
+				}
+
+			}
+			
+		}
+		return enties;
+
+	}
 }
--- src/test/java/stackbuilders/crawler/TestServiceCrawler.java
+++ src/test/java/stackbuilders/crawler/TestServiceCrawler.java
@@ -1,13 +1,7 @@
 package stackbuilders.crawler;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.text.html.parser.Entity;
-
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.junit.Test;
 
 import stackbuilders.crawler.entities.News;
@@ -15,71 +9,17 @@ import stackbuilders.crawler.services.ServiceCrawlerImpl;
 
 public class TestServiceCrawler {
 
-	
-
 	@Test
 	public void getHtmlDocumentTest() {
 		String url = "https://news.ycombinator.com/";
 		ServiceCrawlerImpl crawler = new ServiceCrawlerImpl();
-		Document document = crawler.getHtmlDocument(url);
-		Elements entradas = document.select(".itemlist");
-		System.out.println("Número de entradas en la página inicial de Jarroba: " + entradas.size() + "\n");
-		List<News> enties = new ArrayList<>();
-		boolean agregar;
-		News newTemp = new News();
-		for (Element table : entradas) {
-
-			for (Element row : table.select("tr")) {
-				// String titulo = table.getElementsByClass("storylink").text();
-				Elements tds = row.select("td");
-				
-				agregar= false;
-				for (Element td : tds) {
-					if (td.getElementsByClass("rank").size() > 0) {
-						newTemp = new News();
-						String numberOrder = td.getElementsByClass("rank").text();
-						newTemp.setNumberOrder(Integer.parseInt(numberOrder.replace(".", "").trim()));
-
-					}
-					if (td.getElementsByClass("storylink").size() > 0) {
-
-						String titulo = td.getElementsByClass("storylink").text();
-						newTemp.setTitle(titulo);
-					}
-					if (td.getElementsByClass("score").size() > 0) {
-
-						String score = td.getElementsByClass("score").text();
-						newTemp.setPoints(Integer.parseInt(score.replace("points", "").trim()));
-						 agregar = true;
-					}
-//					if (td.getElementsByClass("storylink").size() > 0) {
-//
-//						String titulo = td.getElementsByClass("storylink").text();
-//						newTemp.setTitle(titulo);
-//					}
-				}
-				if(agregar){
-					enties.add(newTemp);
-				}
-				
-			}
-
-			for (News a : enties) {
-				System.out.println(a.getNumberOrder() + " " + a.getTitle()+ " " + a.getPoints());
-			}
-			// String titulo = table.getElementsByClass("storylink").text();
-			// String autor = table.getElementsByClass("autor").toString();
-			// String fecha = table.getElementsByClass("fecha").text();
-
-			// System.out.println(titulo+"\n"+autor+"\n"+fecha+"\n\n");
-
-			// Con el método "text()" obtengo el contenido que hay dentro de las
-			// etiquetas HTML
-			// Con el método "toString()" obtengo todo el HTML con etiquetas
-			// incluidas
+		List<News> news= crawler.loadNews(url);
+		
+		for (News a : news) {
+			System.out.println(a.getNumberOrder() + " " + a.getTitle() + " " + a.getPoints()+ " " + a.getAmountComments());
 		}
-
-		// assertTrue("someLibraryMethod should return 'true'", );
+		
+		
 	}
 
 }
